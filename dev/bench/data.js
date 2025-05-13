bb22199991c91ee45d779ceb800ea9ec9db7e0a5
github-action-benchmark <github@users.noreply.github.com>
2025-05-13 07:22:00 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 90b7d4bdc89f3301137d08edd31cb8c5049a96d5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747120919144,
+  "lastUpdate": 1747120920204,
   "repoUrl": "https://github.com/saracreates/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -337,6 +337,65 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "90b7d4bdc89f3301137d08edd31cb8c5049a96d5",
+          "message": "Adding RVecs of podio's ObjectID and LinkData (#439)\n\n* Adding RVecs of podio's ObjectID and LinkData\n\n* Turning clang-format off for the file",
+          "timestamp": "2025-04-29T11:41:32+02:00",
+          "tree_id": "b01054e82759d7be1877c882b032c51341cc09c6",
+          "url": "https://github.com/saracreates/FCCAnalyses/commit/90b7d4bdc89f3301137d08edd31cb8c5049a96d5"
+        },
+        "date": 1747120919898,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 1.1525520195873917,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 11.173570055191144,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 2.137665855608518,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 3.887874809642117,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.9049009419888794,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/stages_source.py",
+            "value": 7.416439328489916,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 7.025748406150954,
+            "unit": "Evt/s"
           }
         ]
       }
